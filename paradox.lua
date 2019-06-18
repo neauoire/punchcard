02e310bacf311a1cabda601aeab06ceaeefa78f1
Devine Lu Linvega <aliceffekt@gmail.com>
2019-06-18 16:32:11 +0900

    Added toggle_play

--- paradox.lua
+++ paradox.lua
@@ -1,14 +1,19 @@
--- spacetime
--- norns study 3
+--  
+--   ////\\\\
+--   ////\\\\  PARADOX
+--   ////\\\\  BY NEAUOIRE
+--   \\\\////  
+--   \\\\////  MIDI SEQ
+--   \\\\////
 --
+
+-- spacetime(study-3) remix
+
 -- ENC 1 - sweep filter
 -- ENC 2 - select edit position
 -- ENC 3 - choose command
 -- KEY 3 - randomize command set
---
--- spacetime is a weird function sequencer.
--- it plays a note on each step.
--- each step is a symbol for the action.
+
 -- + = increase note
 -- - = decrease note
 -- < = go to bottom note
@@ -17,14 +22,15 @@
 -- M = fast metro
 -- m = slow metro
 -- # = jump random position
---
--- augment/change this script with new functions!
 
 engine.name = "PolyPerc"
 
+local counter
+local is_playing = true
+
 note = 40
 position = 1
-step = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+step = {1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1}
 STEPS = 16
 edit = 1
 
@@ -42,7 +48,7 @@ COMMANDS = 8
 label = {"+", "-", "<", ">", "*", "M", "m", "#"}
 
 function init()
-  params:add_control("cutoff","cutoff",controlspec.new(50,5000,'exp',0,555,'hz'))
+  params:add_control("cutoff", "cutoff", controlspec.new(50, 5000, 'exp', 0, 555, 'hz'))
   params:set_action("cutoff", function(x) engine.cutoff(x) end)
   counter = metro.init(count, 0.125, -1)
   counter:start()
@@ -57,7 +63,8 @@ end
 
 function redraw()
   screen.clear()
-  for i=1,16 do
+  
+  for i = 1,16 do
     screen.level((i == edit) and 15 or 2)
     screen.move(i*8-8,40)
     screen.text(label[step[i]])
@@ -67,33 +74,46 @@ function redraw()
       screen.stroke()
     end
   end
+  
   screen.update()
 end
 
 function enc(n,d)
   if n == 1 then
-    params:delta("cutoff",d)
+    params:delta("cutoff", d)
   elseif n == 2 then
     edit = util.clamp(edit + d, 1, STEPS)
-  elseif n == 3 then
+  elseif n ==3 then
     step[edit] = util.clamp(step[edit]+d, 1, COMMANDS)
   end
   redraw()
 end
 
 function key(n,z)
-  if n==3 and z==1 then
+  if n == 2 and z == 1 then
+    toggle_play()
+  end
+  if n == 3 and z == 1 then
     randomize_steps()
   end
 end
 
-function midi_to_hz(note)
-  return (440 / 32) * (2 ^ ((note - 9) / 12))
-end
-
-function randomize_steps()
-  for i=1,16 do
-    step[i] = math.random(COMMANDS)
+function toggle_play()
+  if is_playing == true then
+    counter:stop()
+    is_playing = false
+  else
+    counter:start()
+    is_playing = true
   end
 end
 
+function midi_to_hz(note)
+  return (440/32) * (2 ^ ((note - 9) / 12))
+end
+
+function randomize_steps()
+  for i= 1,16 do
+    step[i] = math.random(COMMANDS)
+  end
+end
