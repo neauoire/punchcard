138b363430f0366ef288a166d82470e5cec008ff
Devine Lu Linvega <aliceffekt@gmail.com>
2019-06-25 12:56:27 +0700

    Almost done with client

--- lib/instruct.lua
+++ lib/instruct.lua
@@ -19,35 +19,57 @@ Instruct.make_target = function(self,id)
 end
 
 Instruct.make_type = function(self,id,bin)
-  if char_at(bin,5,2) == '11' then return 'CHANNEL'
-  elseif char_at(bin,5) == '1' then return 'OCTAVE'
-  elseif char_at(bin,6) == '1' then return 'NOTE'
-  else return 'FRAME' end
+  if char_at(bin,5,2) == '11' then return 'RATE'
+  elseif char_at(bin,5) == '1' then return 'FRAME'
+  elseif char_at(bin,6) == '1' then return 'OCTAVE'
+  else return 'NOTE' end
+end
+
+Instruct.make_send_type = function(self,id,bin)
+  if char_at(bin,5,2) == '11' then return 'SYSTEM'
+  elseif char_at(bin,5) == '1' then return 'BANG'
+  elseif char_at(bin,6) == '1' then return 'OSC'
+  else return 'CHANNEL' end
 end
 
 Instruct.make_note = function(self,id,bin)
   key = self:make_number(id,bin)
+  if key == 16 then return 'NEXT+' end
+  if key == 15 then return 'PREV+' end
+  if key == 14 then return 'NEXT#' end
+  if key == 13 then return 'PREV#' end
+  if key == 12 then return 'NEXT^' end
+  if key == 11 then return 'PREV^' end
+  if key == 10 then return 'INCR' end
+  if key ==  9 then return 'DECR' end
   names = { 'C','C#','D','D#','E','F','F#','G','G#','A','A#','B' }
   return names[((key-1) % 12)+1]
 end
 
+Instruct.make_octave = function(self,id,bin)
+  key = self:make_number(id,bin)
+  if key == 10 then return 'INCR' end
+  if key ==  9 then return 'DECR' end
+  return math.floor(((key-1) % 8)+1)
+end
+
 Instruct.make_number = function(self,id,bin)
-  if char_at(bin,1,4) == '0000' then return '1' end
-  if char_at(bin,1,4) == '0001' then return '2' end
-  if char_at(bin,1,4) == '0010' then return '3' end
-  if char_at(bin,1,4) == '0011' then return '4' end
-  if char_at(bin,1,4) == '0100' then return '5' end
-  if char_at(bin,1,4) == '0101' then return '6' end
-  if char_at(bin,1,4) == '0110' then return '7' end
-  if char_at(bin,1,4) == '0111' then return '8' end
-  if char_at(bin,1,4) == '1000' then return '9' end
-  if char_at(bin,1,4) == '1001' then return '10' end
-  if char_at(bin,1,4) == '1010' then return '11' end
-  if char_at(bin,1,4) == '1011' then return '12' end
-  if char_at(bin,1,4) == '1100' then return '13' end
-  if char_at(bin,1,4) == '1101' then return '14' end
-  if char_at(bin,1,4) == '1110' then return '15' end
-  if char_at(bin,1,4) == '1111' then return '16' end
+  if char_at(bin,1,4) == '0000' then return 1 end
+  if char_at(bin,1,4) == '0001' then return 2 end
+  if char_at(bin,1,4) == '0010' then return 3 end
+  if char_at(bin,1,4) == '0011' then return 4 end
+  if char_at(bin,1,4) == '0100' then return 5 end
+  if char_at(bin,1,4) == '0101' then return 6 end
+  if char_at(bin,1,4) == '0110' then return 7 end
+  if char_at(bin,1,4) == '0111' then return 8 end
+  if char_at(bin,1,4) == '1000' then return 9 end
+  if char_at(bin,1,4) == '1001' then return 10 end
+  if char_at(bin,1,4) == '1010' then return 11 end
+  if char_at(bin,1,4) == '1011' then return 12 end
+  if char_at(bin,1,4) == '1100' then return 13 end
+  if char_at(bin,1,4) == '1101' then return 14 end
+  if char_at(bin,1,4) == '1110' then return 15 end
+  if char_at(bin,1,4) == '1111' then return 16 end
   return '0'
 end
 
@@ -65,6 +87,8 @@ Instruct.build_set = function(self,id,bin)
   _type = self:make_type(id,bin)
   if _type == 'NOTE' then
     _value = self:make_note(id,bin)
+  elseif _type == 'OCTAVE' then
+    _value = self:make_octave(id,bin)
   else
     _value = self:make_number(id,bin)
   end
@@ -72,13 +96,9 @@ Instruct.build_set = function(self,id,bin)
 end
 
 Instruct.build_send = function(self,id,bin)
-  _type = self:make_type(id,bin)
-  if _type == 'NOTE' then
-    _value = self:make_note(id,bin)
-  else
-    _value = self:make_number(id,bin)
-  end
-  self.dict[id] = { name = 'SEND '.._type..' TO '.._value }
+  _type = self:make_send_type(id,bin)
+  _value = self:make_number(id,bin)
+  self.dict[id] = { name = 'SEND TO '.._type..' > '.._value }
 end
 
 Instruct.build = function(self)
